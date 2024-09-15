a0f226d328583b10e636ec53e16026f490a5548b
mistergeki07 <mistergeki07@gmail.com>
2024-09-15 13:52:39 +0600

    Доработал сюжет до выбора классов, Добавил абстрактные оружия

--- src/Characters/PersonClass.java
+++ src/Characters/PersonClass.java
@@ -1,5 +1,25 @@
 package Characters;
 
+//класс персонажа
 public enum PersonClass {
+    WARRIOR("Воин"),
+    MAG("Маг"),
+    TANK("Танк");
 
+    private String title;
+
+    PersonClass(String title) {
+        this.title = title;
+    }
+
+    PersonClass() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
---
+++ src/item/attackItem/AbstractSword.java
@@ -0,0 +1,13 @@
+package item.attackItem;
+import item.Weapon;
+
+//Мечи, предмет аттаки
+public abstract class AbstractSword extends Weapon{
+    public AbstractSword() {
+    }
+
+    public void attack() {
+
+    }
+
+}
